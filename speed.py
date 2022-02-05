f29da8a0cd231a5f83399385aad4b6a38bf57b37
8192_rl <aeraulx@gmail.com>
2022-02-05 11:49:14 +0100

    First commit

---
+++ speed.py
@@ -0,0 +1,18 @@
+
+import os
+import time
+import sys
+
+#def: __init__()
+
+durrent_date = time.strftime('%x')
+durrent_date = durrent_date.replace("/","-")
+current_time = time.strftime('%X')
+current_time = current_time.replace(":","-")
+
+file_name=str(durrent_date)+"_speedmonitor_"+str(current_time)+".log"
+
+speedcommand = "speedtest > "+str(file_name);
+
+res = os.system(speedcommand)
+#the method returns the exit status
---
+++ utils.py
@@ -0,0 +1,23 @@
+
+# ========================================
+# ADD TO MAIN LOG
+# ========================================
+# This function add the last values to
+# stored in @param current_file_log to the 
+# @param general_file_log
+
+def word_in_file_replace(current_file_log, general_file_log):
+
+    source_file = open(source_file_name,"r+")
+    new_file = []
+
+    for line in source_file:
+        if word_i in line:
+            line = line.replace(word_i, word_o)
+        new_file.append(line)
+
+    source_file.close()
+    source_file = open(source_file_name,"w+")
+    source_file.writelines(new_file)
+    source_file.close()
+    return;
