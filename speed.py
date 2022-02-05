23bca87ea319c69979571fa404aa0e08ec038c5b
8192_rl <aeraulx@gmail.com>
2022-02-05 20:26:50 +0100

    Arguments version implemented. Basic version.

--- speed.py
+++ speed.py
@@ -1,9 +1,23 @@
-
+import sys
 import os
 import time
 
 from TestDone import TestDone
 import utils
+
+#Reading from CLI
+file_path = ""
+
+if(len(sys.argv)==2):
+    file_path = str(sys.argv[1])
+else: 
+    file_path = ""
+
+if(len(sys.argv)>2):
+    print("Invalid arguments")
+    utils.print_help()
+    exit()
+
 
 #Preparing test
 test = TestDone()
@@ -20,23 +34,24 @@ current_time = current_time.replace(":","-")
 file_name=str(current_date)+"_speedmonitor_"+str(current_time)+".log"
 
 #Preparing OS command
-speedcommand = "speedtest > "+str(file_name);
+speedcommand = "speedtest > "+file_path+str(file_name);
 
 #Launching OS command
 res = os.system(speedcommand)
 
 if(res==0):
-    #Now a log file should be crated.
+    # We'll obtaing ping, download speed and upload speed 
+    # and we'll save the values in TestDone object
+    t = utils.get_test_values(""+file_path+str(file_name))
 
-    #We'll obtaing ping, download speed and upload speed
-    t = utils.get_test_values(file_name)
-
-    print("TEST DONE!\n")
+    #Saving values in TestDone object
     t.date = current_date
     t.time = current_time
     t.ObjectPrint()
+    print("TEST DONE!")
 
-    utils.add_to_main_log("speedmonitor.csv",t)
+    #Joining the last test values in speedmonitor.csv
+    utils.add_to_main_log(""+file_path+"speedmonitor.csv",t)
 else:
     print("TEST FAILED!\n")
 
--- utils.py
+++ utils.py
@@ -3,40 +3,17 @@ import csv
 from TestDone import TestDone
 
 # ==========================================
-# ADD TO MAIN LOG
+# HELP PRINTER
 # ==========================================
-# This function add the new test values to
-# the general log file if it exists
-# if the general log does'nt exist
-# the function creates a new one
-# @param genera_file log -> general file log
-# name
-# @param test -> test object to be added
+# This function prints the rule to call 
+# speed.py
 
-def add_to_main_log(general_file_log, test):
-    file_name = str(general_file_log)
-    fields = ['Date','Time','Ping(ms)','Download speed(Mbit/s)', 'Upload speed(Mbit/s)']
-    row = [test.date,test.time,test.ping,test.speed_download,test.speed_upload]
-    general_log_writer = ""
+def print_help():
+    print("python3 speed.py <log file path>")
+    print("if any log file path is introduced a \".\" path will be used")
 
-    file_exists = exists(general_file_log)
-
-    if(file_exists):
-        #TODO: CHECK IF THE FILE IS CREATED, THEN WRITE OR APPEND...
-        with open(file_name, 'a') as csvfile:
-            # creating a csv reader object
-            general_log_writer = csv.writer(csvfile)
-            print("File: "+file_name+" exists\n")
-            general_log_writer.writerow(row)
-    else:
-        with open(file_name, 'w') as csvfile:
-            # creating a csv reader object
-            general_log_writer = csv.writer(csvfile)
-            print("File: "+file_name+" doesn't exist")
-            print("File: "+file_name+" created\n")
-            general_log_writer.writerow(fields)
-            general_log_writer.writerow(row)
-
+#TODO: IMPLEMENT THE FUNCTION
+def path_check(file_path):
     return
 
 # ==========================================
@@ -54,24 +31,65 @@ def get_test_values(current_file_log):
 
     test = TestDone()
 
-    line_hosted = ""
-    line_download = ""
-    line_upload = ""
+    line_hosted     = ""
+    line_download   = ""
+    line_upload     = ""
 
     for line in current_file:
         if("Hosted") in line:
-            line_hosted = line;
+            line_hosted     = line;
         if("Download") in line:
-            line_download = line
+            line_download   = line
         if("Upload") in line:
-            line_upload = line
+            line_upload     = line
 
-    line_hosted = line_hosted.split(" ")
-    line_download = line_download.split(" ")
-    line_upload = line_upload.split(" ")
+    line_hosted         = line_hosted.split(" ")
+    line_download       = line_download.split(" ")
+    line_upload         = line_upload.split(" ")
 
     test.ping           = line_hosted[len(line_hosted)-2]
     test.speed_download = line_download[len(line_download)-2]
     test.speed_upload   = line_upload[len(line_upload)-2]
 
     return test
+
+
+# ==========================================
+# ADD TO MAIN LOG
+# ==========================================
+# This function add the new test values to
+# the general log file if it exists
+# if the general log does'nt exist
+# the function creates a new one
+# @param genera_file log -> general file log
+# name
+# @param test -> test object to be added
+
+def add_to_main_log(general_file_log, test):
+    #Defining and preparing variables
+    file_name = str(general_file_log)
+    fields = ['Date','Time','Ping(ms)','Download speed(Mbit/s)', 'Upload speed(Mbit/s)']
+    row = [test.date,test.time,test.ping,test.speed_download,test.speed_upload]
+    general_log_writer = ""
+
+    #Checking if the general file exists
+    file_exists = exists(general_file_log)
+
+    if(file_exists):
+        #if it exists we append a new row
+        with open(file_name, 'a') as csvfile:
+            # creating a csv reader object
+            general_log_writer = csv.writer(csvfile)
+            print("File: "+file_name+" exists\n")
+            general_log_writer.writerow(row)
+    else:
+        #if it does'nt exist we create a new one and add the values obtained in the test
+        with open(file_name, 'w') as csvfile:
+            # creating a csv reader object
+            general_log_writer = csv.writer(csvfile)
+            print("File: "+file_name+" doesn't exist")
+            print("File: "+file_name+" created\n")
+            general_log_writer.writerow(fields)
+            general_log_writer.writerow(row)
+
+    return
